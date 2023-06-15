6dbcddd437a01ff214fb5b327a22772e9802be1f
Tobias Muñoz <81777964+tobiasmunoz@users.noreply.github.com>
2023-06-15 12:46:57 -0300

    Update clase21.R

--- ejerciciosEnClase/clase21.R
+++ ejerciciosEnClase/clase21.R
@@ -38,7 +38,37 @@ intervalo_conf <- c( modelo$coefficients[2] - sqrt(sigma2*denominador)*qt(1-0.05
 
 # f) Informar la significatividad de las variables, indicando cada uno de los tests que devuelve el
 # resumen ( summary ) del ajuste del item b). ¿Como se calcula el p-valor en cada test?
+# Rta: coeficiente mas grande en apgar5, tiene mayor significatividad que edadG. P-valores muy bajos y casi nulo para apgar5
 
 # g) ¿Cuales supuestos tuvo que asumir para responder los tres items anteriores?
+# Rta:  Normalidad e i.i.d. de los errores ϵ
 
 # h) Comentar la magnitud de R2
+# Rta: R2 = 0.55 No demuestra que el ajuste es muy bueno ni muy malo.
+
+# Calculo de los coeficientes, t-value, p-value, R-squared, F-statistic, residuos, etc.
+
+coeficientes <- solve( t(X) %*% X ) %*% t(X) %*% Y
+coeficientes
+
+Y_hat <- X %*% coeficientes
+residuos <- Y - Y_hat
+res_se <- sqrt( sum(residuos^2) / (100-3) ) # Residual Standard Error
+res_se
+
+# dii = elemento de la diagonal de la matriz t(a) * ( t(x) * X )^-1 * a
+d11 <- t( matrix(data = c(0,1,0)) ) %*% solve( t(X) %*% X ) %*% matrix(data = c(0,1,0))
+d22 <- t( matrix(data = c(0,0,1)) ) %*% solve( t(X) %*% X ) %*% matrix(data = c(0,0,1))
+
+t_edadG <- coeficientes[2] / (res_se * sqrt(d11))
+t_edadG
+t_apgar5 <- coeficientes[3] / (res_se * sqrt(d22))
+t_apgar5
+
+p_value_edadG <- 1 - pt(t_edadG, 100-3)
+p_value_edadG
+p_value_apgar5 <- 1 - pt(t_apgar5, 100-3)
+p_value_apgar5
+
+r_squared <- sum( ( Y_hat - mean(Y) )^2 ) / sum( ( Y - mean(Y) )^2 )
+r_squared
